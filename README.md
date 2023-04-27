545911f8b7644fe1e4199fdf233b26e37b077ff3
赖申其 <laishenqi@laishenqideMacBook-Pro.local>
2023-04-27 11:21:13 +0800

    update result

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
 # A PyTorch implementation of MobileNetV3
-I make a mistake to forget the avgpool in se model, now I have re-trained the mbv3_small, the mbv3_large is on training, it will be coming soon.
+I retrain the mobilenetv3 with some novel tricks and [timm](https://github.com/huggingface/pytorch-image-models). 
+I also provide the train code, pre-training weight and training logs on this project. 
 
 You should use torch.load to load the model.
-
 ```
-model = torch.load("mbv3_large.old.pth.tar", map_location='cpu')
-weight = model["state_dict"]
+from mobilenetv3 import MobileNetV3_Small, MobileNetV3_Large
+
+# MobileNetV3_Small
+net = MobileNetV3_Small()
+net.load_state_dict(torch.load("450_act3_mobilenetv3_small.pth", map_location='cpu'))
+
+# MobileNetV3_Large
+net = MobileNetV3_Large()
+net.load_state_dict(torch.load("450_act3_mobilenetv3_large.pth", map_location='cpu'))
+```
+
+You could reproduce the model by the code.
+```
+nohup python -u -m torch.distributed.run --nproc_per_node=8 main.py --model mobilenet_v3_small --epochs 300 --batch_size 256 --lr 4e-3 --update_freq 2 --model_ema false --model_ema_eval false --use_amp true --data_path /data/benchmarks/ILSVRC2012 --output_dir ./checkpoint &
+
+nohup python -u -m torch.distributed.run --nproc_per_node=8 main.py --model mobilenet_v3_small --epochs 450 --batch_size 256 --lr 4e-3 --update_freq 2 --model_ema false --model_ema_eval false --use_amp true --data_path /data/benchmarks/ILSVRC2012 --output_dir ./checkpoint &
+
+nohup python -u -m torch.distributed.run --nproc_per_node=8 main.py --model mobilenet_v3_large --epochs 300 --batch_size 256 --lr 4e-3 --update_freq 2 --model_ema false --model_ema_eval false --use_amp true --data_path /data/benchmarks/ILSVRC2012 --output_dir ./checkpoint &
+
+nohup python -u -m torch.distributed.run --nproc_per_node=8 main.py --model mobilenet_v3_large --epochs 450 --batch_size 256 --lr 4e-3 --update_freq 2 --model_ema false --model_ema_eval false --use_amp true --data_path /data/benchmarks/ILSVRC2012 --output_dir ./checkpoint &
 ```
 
 This is a PyTorch implementation of MobileNetV3 architecture as described in the paper [Searching for MobileNetV3](https://arxiv.org/pdf/1905.02244.pdf).
@@ -13,11 +31,13 @@ This is a PyTorch implementation of MobileNetV3 architecture as described in the
 Some details may be different from the original paper, welcome to discuss and help me figure it out.
 
 ### MobileNetV3
-|              | Madds     | Parameters | Top1-acc  |
-| -----------  | --------- | ---------- | --------- |
-| Large        | 219 M     | 5.4  M     | 75.2%     |
-| Small        | 66  M     | 2.9  M     | 67.4%     |
-| Ours Large old   | 272 M     | 3.96  M     | 75.454%   |
-| Ours Small old   | 66  M     | 2.51  M     | 69.069%   |
-| Ours Large new   | 265 M     | 3.96  M     |   |
-| Ours Small new   | 64  M     | 2.51  M     | 69.037%   |
+|                       | Madds     | Parameters | Top1-acc  |
+| -------------------   | --------- | ---------- | --------- |
+| Small (paper)         | 66  M     | 2.9 M      | 67.4%     |
+| Small (torchvision)   | 62  M     | 2.5 M      | 67.7%     |
+| Small (our 300 epoch) | 69  M     | 3.0 M      | 68.9%     |
+| Small (our 450 epoch) | 69  M     | 3.0 M      | 69.2%     |
+| Large (paper)         | 219  M    | 5.4 M      | 75.2%     |
+| Large (torchvision)   | 235  M    | 5.5 M      | 74.0%     |
+| Large (our 300 epoch) | 241  M    | 5.2 M      | 75.6%     |
+| Large (our 450 epoch) | 241  M    | 5.2 M      | 75.9%     |
--- mobilenetv3.py
+++ mobilenetv3.py
@@ -3,7 +3,6 @@
 See the paper "Inverted Residuals and Linear Bottlenecks:
 Mobile Networks for Classification, Detection and Segmentation" for more details.
 '''
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import init
@@ -25,14 +24,14 @@ class hsigmoid(nn.Module):
 class SeModule(nn.Module):
     def __init__(self, in_size, reduction=4):
         super(SeModule, self).__init__()
+        expand_size =  max(in_size // reduction, 8)
         self.se = nn.Sequential(
             nn.AdaptiveAvgPool2d(1),
-            nn.Conv2d(in_size, in_size // reduction, kernel_size=1, stride=1, padding=0, bias=False),
-            nn.BatchNorm2d(in_size // reduction),
+            nn.Conv2d(in_size, expand_size, kernel_size=1, bias=False),
+            nn.BatchNorm2d(expand_size),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_size // reduction, in_size, kernel_size=1, stride=1, padding=0, bias=False),
-            nn.BatchNorm2d(in_size),
-            hsigmoid()
+            nn.Conv2d(expand_size, in_size, kernel_size=1, bias=False),
+            nn.Hardsigmoid()
         )
 
     def forward(self, x):
@@ -41,126 +40,89 @@ class SeModule(nn.Module):
 
 class Block(nn.Module):
     '''expand + depthwise + pointwise'''
-    def __init__(self, kernel_size, in_size, expand_size, out_size, nolinear, semodule, stride):
+    def __init__(self, kernel_size, in_size, expand_size, out_size, act, se, stride):
         super(Block, self).__init__()
         self.stride = stride
-        self.se = semodule
 
-        self.conv1 = nn.Conv2d(in_size, expand_size, kernel_size=1, stride=1, padding=0, bias=False)
+        self.conv1 = nn.Conv2d(in_size, expand_size, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(expand_size)
-        self.nolinear1 = nolinear
+        self.act1 = act(inplace=True)
+
         self.conv2 = nn.Conv2d(expand_size, expand_size, kernel_size=kernel_size, stride=stride, padding=kernel_size//2, groups=expand_size, bias=False)
         self.bn2 = nn.BatchNorm2d(expand_size)
-        self.nolinear2 = nolinear
-        self.conv3 = nn.Conv2d(expand_size, out_size, kernel_size=1, stride=1, padding=0, bias=False)
-        self.bn3 = nn.BatchNorm2d(out_size)
+        self.act2 = act(inplace=True)
+        self.se = SeModule(expand_size) if se else nn.Identity()
 
-        self.shortcut = nn.Sequential()
+        self.conv3 = nn.Conv2d(expand_size, out_size, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_size)
+        self.act3 = act(inplace=True)
+
+        self.skip = None
         if stride == 1 and in_size != out_size:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_size, out_size, kernel_size=1, stride=1, padding=0, bias=False),
-                nn.BatchNorm2d(out_size),
+            self.skip = nn.Sequential(
+                nn.Conv2d(in_size, out_size, kernel_size=1, bias=False),
+                nn.BatchNorm2d(out_size)
+            )
+
+        if stride == 2 and in_size != out_size:
+            self.skip = nn.Sequential(
+                nn.Conv2d(in_channels=in_size, out_channels=in_size, kernel_size=3, groups=in_size, stride=2, padding=1, bias=False),
+                nn.BatchNorm2d(in_size),
+                nn.Conv2d(in_size, out_size, kernel_size=1, bias=True),
+                nn.BatchNorm2d(out_size)
+            )
+
+        if stride == 2 and in_size == out_size:
+            self.skip = nn.Sequential(
+                nn.Conv2d(in_channels=in_size, out_channels=out_size, kernel_size=3, groups=in_size, stride=2, padding=1, bias=False),
+                nn.BatchNorm2d(out_size)
             )
 
     def forward(self, x):
-        out = self.nolinear1(self.bn1(self.conv1(x)))
-        out = self.nolinear2(self.bn2(self.conv2(out)))
+        skip = x
+
+        out = self.act1(self.bn1(self.conv1(x)))
+        out = self.act2(self.bn2(self.conv2(out)))
+        out = self.se(out)
         out = self.bn3(self.conv3(out))
-        if self.se != None:
-            out = self.se(out)
-        out = out + self.shortcut(x) if self.stride==1 else out
-        return out
-
-
-class MobileNetV3_Large(nn.Module):
-    def __init__(self, num_classes=1000):
-        super(MobileNetV3_Large, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.hs1 = hswish()
-
-        self.bneck = nn.Sequential(
-            Block(3, 16, 16, 16, nn.ReLU(inplace=True), None, 1),
-            Block(3, 16, 64, 24, nn.ReLU(inplace=True), None, 2),
-            Block(3, 24, 72, 24, nn.ReLU(inplace=True), None, 1),
-            Block(5, 24, 72, 40, nn.ReLU(inplace=True), SeModule(40), 2),
-            Block(5, 40, 120, 40, nn.ReLU(inplace=True), SeModule(40), 1),
-            Block(5, 40, 120, 40, nn.ReLU(inplace=True), SeModule(40), 1),
-            Block(3, 40, 240, 80, hswish(), None, 2),
-            Block(3, 80, 200, 80, hswish(), None, 1),
-            Block(3, 80, 184, 80, hswish(), None, 1),
-            Block(3, 80, 184, 80, hswish(), None, 1),
-            Block(3, 80, 480, 112, hswish(), SeModule(112), 1),
-            Block(3, 112, 672, 112, hswish(), SeModule(112), 1),
-            Block(5, 112, 672, 160, hswish(), SeModule(160), 1),
-            Block(5, 160, 672, 160, hswish(), SeModule(160), 2),
-            Block(5, 160, 960, 160, hswish(), SeModule(160), 1),
-        )
-
-
-        self.conv2 = nn.Conv2d(160, 960, kernel_size=1, stride=1, padding=0, bias=False)
-        self.bn2 = nn.BatchNorm2d(960)
-        self.hs2 = hswish()
-        self.linear3 = nn.Linear(960, 1280)
-        self.bn3 = nn.BatchNorm1d(1280)
-        self.hs3 = hswish()
-        self.linear4 = nn.Linear(1280, num_classes)
-        self.init_params()
-
-    def init_params(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                init.kaiming_normal_(m.weight, mode='fan_out')
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                init.constant_(m.weight, 1)
-                init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                init.normal_(m.weight, std=0.001)
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-
-    def forward(self, x):
-        out = self.hs1(self.bn1(self.conv1(x)))
-        out = self.bneck(out)
-        out = self.hs2(self.bn2(self.conv2(out)))
-        out = F.avg_pool2d(out, 7)
-        out = out.view(out.size(0), -1)
-        out = self.hs3(self.bn3(self.linear3(out)))
-        out = self.linear4(out)
-        return out
+        
+        if self.skip is not None:
+            skip = self.skip(skip)
+        return self.act3(out + skip)
 
 
 
 class MobileNetV3_Small(nn.Module):
-    def __init__(self, num_classes=1000):
+    def __init__(self, num_classes=1000, act=nn.Hardswish):
         super(MobileNetV3_Small, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(16)
-        self.hs1 = hswish()
+        self.hs1 = act(inplace=True)
 
         self.bneck = nn.Sequential(
-            Block(3, 16, 16, 16, nn.ReLU(inplace=True), SeModule(16), 2),
-            Block(3, 16, 72, 24, nn.ReLU(inplace=True), None, 2),
-            Block(3, 24, 88, 24, nn.ReLU(inplace=True), None, 1),
-            Block(5, 24, 96, 40, hswish(), SeModule(40), 2),
-            Block(5, 40, 240, 40, hswish(), SeModule(40), 1),
-            Block(5, 40, 240, 40, hswish(), SeModule(40), 1),
-            Block(5, 40, 120, 48, hswish(), SeModule(48), 1),
-            Block(5, 48, 144, 48, hswish(), SeModule(48), 1),
-            Block(5, 48, 288, 96, hswish(), SeModule(96), 2),
-            Block(5, 96, 576, 96, hswish(), SeModule(96), 1),
-            Block(5, 96, 576, 96, hswish(), SeModule(96), 1),
+            Block(3, 16, 16, 16, nn.ReLU, True, 2),
+            Block(3, 16, 72, 24, nn.ReLU, False, 2),
+            Block(3, 24, 88, 24, nn.ReLU, False, 1),
+            Block(5, 24, 96, 40, act, True, 2),
+            Block(5, 40, 240, 40, act, True, 1),
+            Block(5, 40, 240, 40, act, True, 1),
+            Block(5, 40, 120, 48, act, True, 1),
+            Block(5, 48, 144, 48, act, True, 1),
+            Block(5, 48, 288, 96, act, True, 2),
+            Block(5, 96, 576, 96, act, True, 1),
+            Block(5, 96, 576, 96, act, True, 1),
         )
 
 
         self.conv2 = nn.Conv2d(96, 576, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn2 = nn.BatchNorm2d(576)
-        self.hs2 = hswish()
-        self.linear3 = nn.Linear(576, 1280)
+        self.hs2 = act(inplace=True)
+        self.gap = nn.AdaptiveAvgPool2d(1)
+
+        self.linear3 = nn.Linear(576, 1280, bias=False)
         self.bn3 = nn.BatchNorm1d(1280)
-        self.hs3 = hswish()
+        self.hs3 = act(inplace=True)
+        self.drop = nn.Dropout(0.2)
         self.linear4 = nn.Linear(1280, num_classes)
         self.init_params()
 
@@ -181,19 +143,73 @@ class MobileNetV3_Small(nn.Module):
     def forward(self, x):
         out = self.hs1(self.bn1(self.conv1(x)))
         out = self.bneck(out)
+
         out = self.hs2(self.bn2(self.conv2(out)))
-        out = F.avg_pool2d(out, 7)
-        out = out.view(out.size(0), -1)
-        out = self.hs3(self.bn3(self.linear3(out)))
-        out = self.linear4(out)
-        return out
+        out = self.gap(out).flatten(1)
+        out = self.drop(self.hs3(self.bn3(self.linear3(out))))
+
+        return self.linear4(out)
 
 
+class MobileNetV3_Large(nn.Module):
+    def __init__(self, num_classes=1000, act=nn.Hardswish):
+        super(MobileNetV3_Large, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.hs1 = act(inplace=True)
 
-def test():
-    net = MobileNetV3_Small()
-    x = torch.randn(2,3,224,224)
-    y = net(x)
-    print(y.size())
+        self.bneck = nn.Sequential(
+            Block(3, 16, 16, 16, nn.ReLU, False, 1),
+            Block(3, 16, 64, 24, nn.ReLU, False, 2),
+            Block(3, 24, 72, 24, nn.ReLU, False, 1),
+            Block(5, 24, 72, 40, nn.ReLU, True, 2),
+            Block(5, 40, 120, 40, nn.ReLU, True, 1),
+            Block(5, 40, 120, 40, nn.ReLU, True, 1),
+            Block(3, 40, 240, 80, act, False, 2),
+            Block(3, 80, 200, 80, act, False, 1),
+            Block(3, 80, 184, 80, act, False, 1),
+            Block(3, 80, 184, 80, act, False, 1),
+            Block(3, 80, 480, 112, act, True, 1),
+            Block(3, 112, 672, 112, act, True, 1),
+            Block(5, 112, 672, 160, act, True, 2),
+            Block(5, 160, 672, 160, act, True, 1),
+            Block(5, 160, 960, 160, act, True, 1),
+        )
 
-# test()
+
+        self.conv2 = nn.Conv2d(160, 960, kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn2 = nn.BatchNorm2d(960)
+        self.hs2 = act(inplace=True)
+        self.gap = nn.AdaptiveAvgPool2d(1)
+        
+        self.linear3 = nn.Linear(960, 1280, bias=False)
+        self.bn3 = nn.BatchNorm1d(1280)
+        self.hs3 = act(inplace=True)
+        self.drop = nn.Dropout(0.2)
+
+        self.linear4 = nn.Linear(1280, num_classes)
+        self.init_params()
+
+    def init_params(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                init.normal_(m.weight, std=0.001)
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        out = self.hs1(self.bn1(self.conv1(x)))
+        out = self.bneck(out)
+
+        out = self.hs2(self.bn2(self.conv2(out)))
+        out = self.gap(out).flatten(1)
+        out = self.drop(self.hs3(self.bn3(self.linear3(out))))
+        
+        return self.linear4(out)
